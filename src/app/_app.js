34a372b854b0c892b34db17255e1de9f891bf481
Krista Lawson <kristaspielman4311@gmail.com>
2023-07-11 16:23:07 -0400

    simple front-page template

--- src/app/_app.js
+++ src/app/_app.js
@@ -1,10 +1,10 @@
 // import Font Awesome CSS
-import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
-// Tell Font Awesome to skip adding the CSS automatically 
+// Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
-config.autoAddCss = false; 
+config.autoAddCss = false;
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,7 +12,12 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+        },
+        colors: {
+          'pumpColor': '#e5a23d',
+          'cityColor': '#57a5dd',
+          'barrelColor': '#76db5c',
+        }
     },
   },
   plugins: [],
