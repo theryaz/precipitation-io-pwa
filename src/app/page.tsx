9f708557bcadca2dc5361521010a714c78842cfa
Krista Lawson <kristaspielman4311@gmail.com>
2023-07-11 16:34:08 -0400

    isolate sections to seperate components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-4">
       
-      <WateringToggles></WateringToggles>
-      <BarrelStats></BarrelStats>
+      <WateringToggles/>
+      <BarrelStats/>
 
     </main>
   )
